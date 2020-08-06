e870a1b1ca4541895adabecad858024c7f16c894
yashraj2312 <yashrajsrivastava2312@gmail.com>
2020-08-06 16:05:59 +0530

    notes edit

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -11,7 +11,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { 
         id: 11,
         title: 'PayO',
-        description: 'PayO will become Unicorn soon',
+        description: 'PayO will become Unicorn startup soon',
         isDone: false,
         isArchived: false,
       },
